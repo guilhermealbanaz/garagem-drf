071df029577d4c198dacc4c0cb2f4735ecc9c162
Guilherme Bragato Albanaz <guibragatoalbnz@gmail.com>
2022-08-25 14:50:21 -0300

    ultima aula antes do heroku

--- garagem/settings.py
+++ garagem/settings.py
@@ -1,3 +1,4 @@
+import os
 """
 Django settings for garagem project.
 
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'garagem.urls'
@@ -123,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
